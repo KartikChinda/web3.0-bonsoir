4ac5c7e621acd7a5645980dde6ac466f317b7e30
Kartik Chinda <kartik.chinda@gmail.com>
2024-07-02 19:39:17 +0530

    Added the deploy scripts and the hardhat configs

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,16 @@
+require('dotenv').config()
+
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+console.log(process.env.ACCOUNT_KEY)
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/5NEVtFEGlcxy5G1WSJCkVg55w8-pc1mo',
+      accounts: [process.env.ACCOUNT_KEY]
+
+    }
+  }
 };
